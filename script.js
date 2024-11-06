75e2f27d9d5332ca02ea68ba30c97709711f5bd9
Soham-coder397 <sohamghosh2925@gmail.com>
2024-11-07 02:59:12 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'DIWALI', 'MY LOVE♡' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'RAHUL♡' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
